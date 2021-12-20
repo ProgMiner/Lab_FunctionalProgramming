1b67c0f546bd909b0cc93f4940809f85a10fff0e
Eridan Domoratskiy <eridan200@mail.ru>
2021-12-21 02:49:44 +0300

    Refactor code

--- lab3/main.rkt
+++ lab3/main.rkt
@@ -42,17 +42,16 @@
 
   (lambda (next-point) (next-fn next-point (cons first-point null))))
 
-(define (generate-xs [step 1])
-  (define (gen-list from to)
-    (if (> from to) null (cons from (gen-list (+ from step) to))))
+(define (generate-xs step)
+  (define (generate-from x-from)
+    (lambda (x-to)
+      (let* ([xs (inclusive-range x-from x-to step)]
+             ; if we cannot generate any point at this moment
+             ; we would like to start with current state next time
+             [next-x-from (if (empty? xs) x-from (+ (last xs) step))])
+        (cons (generate-from next-x-from) xs))))
 
-  (define (generate-xs first-x)
-    (lambda (next-x)
-      (let* ([xs (gen-list first-x next-x)]
-             [next-x (if (empty? xs) first-x (+ (last xs) step))])
-        (cons (generate-xs next-x) xs))))
-
-  generate-xs)
+  generate-from)
 
 (define (split-seq l) (sequence-map string-split l))
 (define (numbers-seq l) (sequence-map (lambda (xs) (map (compose1 inexact->exact string->number) xs)) l))
@@ -65,36 +64,52 @@
   (map (lambda (x) (cons x (f x))) xs))
 
 (define (handle-stdin approx generate-xs offset)
+  ; fold state
+  ;
+  ; approx - next approx function
+  ; generate-xs - next xs generation function
+  ; prev-iteration - result of previous approximation step,
+  ;                  pair of confiugred approx function and x of last consumed point
+  (struct state (approx generate-xs prev-iteration))
+
+  (define (handle-point st p)
+    (let* ([approx-result ((state-approx st) p)]
+           [next-x-to (if (procedure? approx-result) (car p) (- (car p) offset))]
+           [generate-xs-result ((state-generate-xs st) next-x-to)])
+
+      ; if approx factory returned procedure instead of pair,
+      ; it still is not configured for approximation (e.g. after first point)
+      ; and we cannot produce output
+      (if (procedure? approx-result)
+        (state approx-result generate-xs-result (void))
+        (let ([next-approx (car approx-result)]
+              [next-generate-xs (car generate-xs-result)]
+              [approx-fn (cdr approx-result)]
+              [xs (cdr generate-xs-result)])
+          (for-each yield (map-zip approx-fn xs))
+          (state next-approx next-generate-xs (cons approx-fn (car p)))))))
+
+  (define process-stdin
+    (compose1
+      points-seq
+      filter-points-seq
+      filter-numbers-seq
+      numbers-seq
+      split-seq))
+
   (in-generator
-    (let ([acc
-            (sequence-fold
-              (lambda (acc p)
-                (let* ([approx (list-ref acc 0)]
-                       [generate-xs (list-ref acc 1)]
-                       [approx-result (approx p)]
-                       [generate-xs-result (generate-xs (if (procedure? approx-result) (car p)
-                                                          (- (car p) offset)))])
-                  (if (procedure? approx-result)
-                    (list approx-result generate-xs-result (void))
-                    (let ([next-approx (car approx-result)]
-                          [next-generate-xs (car generate-xs-result)]
-                          [approx-fn (cdr approx-result)]
-                          [xs (cdr generate-xs-result)])
-                      (for-each yield (map-zip approx-fn xs))
-                      (list next-approx next-generate-xs (cons approx-fn (car p)))))))
-              (list approx generate-xs (void))
-              ((compose1
-                 points-seq
-                 filter-points-seq
-                 filter-numbers-seq
-                 numbers-seq
-                 split-seq) (in-lines)))])
-      (if (void? (list-ref acc 2))
-        (void)
-        (let ([approx-fn (car (list-ref acc 2))]
-              [last-x (cdr (list-ref acc 2))]
-              [generate-xs (list-ref acc 1)])
-        (for-each yield (map-zip approx-fn (cdr (generate-xs last-x)))))))))
+    (let ([st (sequence-fold
+                handle-point
+                (state approx generate-xs (void))
+                (process-stdin (in-lines)))])
+
+      ; if we made enough steps to achieve some configured approx function,
+      ; we need to approximate tail points, that could remain if offset > 0
+      (unless (void? (state-prev-iteration st))
+        (let* ([approx-fn (car (state-prev-iteration st))]
+               [last-x (cdr (state-prev-iteration st))]
+               [last-xs (cdr ((state-generate-xs st) last-x))])
+          (for-each yield (map-zip approx-fn last-xs)))))))
 
 (define (display-point p)
   (display (exact->inexact (car p)))
